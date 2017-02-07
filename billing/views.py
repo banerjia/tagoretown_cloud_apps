a5cf090d3c6ab925ed2a3ff175831f1c62244cdf
Abhishek Banerji <webdesign.banerji@gmail.com>
2017-02-06 22:49:51 -0500

    Forms
    Basic form implementation.

--- billing/views.py
+++ billing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404,render
-
+from django.http import HttpResponseRedirect
 from .models import Invoice
+from .forms import NewInvoiceForm
 
 def select_customer(request):
     pass
@@ -16,17 +17,31 @@ def invoices(request, oam_url_part):
         'customer_invoice_list': customer_invoice_list,
         'oam_url_part': oam_url_part
     }
-    return render(request, 'billing/index.html', context)
+    return render(request, 'billing/invoices.html', context)
 
 def invoice_detail(request, oam_url_part, invoice_number):
     queryset = Invoice.objects.filter(customer__oam_url_part = oam_url_part, number = invoice_number)[:1]
     invoice = get_object_or_404(queryset)
     return render( request,
-                  'billing/invoice.html',
+                  'billing/invoice_detail.html',
                   {
                       'oam_url_part': oam_url_part,
                       'invoice': invoice
                   })
+
+def new_invoice(request, oam_url_part):
+    if request.method == "POST":
+        form = NewInvoiceForm(request)
+        if form.is_valid():
+            return HttpResponseRedirect('http://www.cnn.com')
+    else:
+        form = NewInvoiceForm()
+    return render( request,
+                   'billing/new_invoice.html',
+                   {
+                        'oam_url_part': oam_url_part,
+                        'form': form,
+                   })
 
 def invoice_pay(request, oam_url_part, invoice_number):
     pass
